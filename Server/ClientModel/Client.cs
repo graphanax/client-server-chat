b6020708a7077c9bbf226d241269c2ea92a158b9
Artyom <frolov.artyom.main@gmail.com>
2021-04-24 13:59:38 +0300

    Add client model

---
+++ Server/ClientModel/Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Server.ClientModel
+{
+    [Serializable]
+    public abstract class Client
+    {
+        public int Id { get; protected set; }
+        public string Login { get; protected set; }
+    }
+}
---
+++ Server/ClientModel/ConnectedClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Server.ClientModel
+{
+    [Serializable]
+    public sealed class ConnectedClient : Client
+    {
+        public ConnectedClient(string source, string login)
+        {
+            Source = source ?? throw new ArgumentNullException(nameof(source));
+            Login = login ?? throw new ArgumentNullException(nameof(login));
+        }
+
+        public string Source { get; set; }
+    }
+}
